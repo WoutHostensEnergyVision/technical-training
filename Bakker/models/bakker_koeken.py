434c549e5a6144aaba09482027992825273a26b9
Wout Hostens <wout.hostens@energyvision.be>
2025-09-16 10:13:30 +0200

    added some extra fields

--- Bakker/models/bakker_koeken.py
+++ Bakker/models/bakker_koeken.py
@@ -7,3 +7,15 @@ class BakkerKoeken(models.Model):
     name_koek = fields.Char(string="Naam van de koek", required=True)
     prijs_koek = fields.Float(string="Prijs van de koek", required=True)
     voorraad_koek = fields.Integer(string="Voorraad van de koek", required=True)
+    vervaldatum_koek = fields.Date(string="Vervaldatum van de koek", required=True)
+    categorie_koek = fields.Selection(
+        string="Categorie van de koek",
+        selection=[
+            ('chocolade', 'Chocolade'),
+            ('fruit', 'Fruit'),
+            ('noten', 'Noten'),
+            ('speculaas', 'Speculaas'),
+        ],
+        required=True,
+    )
+    pudding_koek = fields.Boolean(string="Bevat pudding", default=False)
