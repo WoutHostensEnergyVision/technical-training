8aa2087221cc6e6acc5284df512d31f71af4d88e
Wout Hostens <wout.hostens@energyvision.be>
2025-09-16 10:08:25 +0200

    fix typeo

--- Bakker/models/bakker_koeken.py
+++ Bakker/models/bakker_koeken.py
@@ -4,6 +4,6 @@ class BakkerKoeken(models.Model):
     _name = "bakker_koeken"
     _description = "Bakker zijn lekkere koeken"
 
-    name_koek = models.CharField(string="Naam van de koek", required=True)
+    name_koek = models.Char(string="Naam van de koek", required=True)
     prijs_koek = models.Float(string="Prijs van de koek", required=True)
     voorraad_koek = models.Integer(string="Voorraad van de koek", required=True)
