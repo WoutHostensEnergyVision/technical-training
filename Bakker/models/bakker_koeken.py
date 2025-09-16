d859522daccdfd17e470cd3152afdefff0973da1
Wout Hostens <wout.hostens@energyvision.be>
2025-09-16 09:55:42 +0200

    Auto commit Tue Sep 16 09:55:42 CEST 2025

--- Bakker/models/bakker_koeken.py
+++ Bakker/models/bakker_koeken.py
@@ -3,3 +3,7 @@ from odoo import models
 class BakkerKoeken(models.Model):
     _name = "bakker_koeken"
     _description = "Bakker zijn lekkere koeken"
+
+    name_koek = models.CharField(string="Naam van de koek", required=True)
+    prijs_koek = models.Float(string="Prijs van de koek", required=True)
+    voorraad_koek = models.Integer(string="Voorraad van de koek", required=True)
