33aad8e2f9719af4e4c03e764144738037b1d31b
Wout Hostens <wout.hostens@energyvision.be>
2025-09-16 15:49:57 +0200

    added tags

--- Bakker/__manifest__.py
+++ Bakker/__manifest__.py
@@ -4,6 +4,7 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
+        "data/bakker_koeken_tags_data.xml",
         "data/bakker_koeken_data.xml",
         "security/ir.model.access.csv",
         "views/bakker_koeken_views.xml",
--- Bakker/models/bakker_koeken_tags.py
+++ Bakker/models/bakker_koeken_tags.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-class Bakker(models.Model):
-    name = models.CharField(max_length=255)
+from odoo import models, fields
+
+class BakkerKoekenTags(models.Model):
+    _name = "bakker_koeken_tags"
+    _description = "Tags voor bakker koeken"
+
+    name = fields.Char(string="Tag naam", required=True)
+    color = fields.Integer(string="Kleur")
 
     def __str__(self):
         return self.name
-
-class KoekenTag(models.Model):
-    bakker = models.ForeignKey(Bakker, on_delete=models.CASCADE, related_name='koeken_tags')
-    tag_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.tag_name
