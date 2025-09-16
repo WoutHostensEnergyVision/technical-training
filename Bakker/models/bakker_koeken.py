591f13aa5a80083a7c95fc1b274824ea19ba049f
Wout Hostens <wout.hostens@energyvision.be>
2025-09-16 11:44:57 +0200

    removed calculated field

--- Bakker/models/bakker_koeken.py
+++ Bakker/models/bakker_koeken.py
@@ -8,7 +8,7 @@ class BakkerKoeken(models.Model):
     name_koek = fields.Char(string="Naam van de koek", required=True, help="Vul hier de naam van de koek in")
     prijs_koek = fields.Float(string="Prijs van de koek", required=True, help="Vul hier de prijs van de koek in")
     voorraad_koek = fields.Integer(string="Voorraad van de koek", required=True , default=10)
-    vervaldatum_koek = fields.Date(string="Vervaldatum van de koek", required=True, help="Vul hier de vervaldatum van de koek in", default=lambda self: fields.Date.today() + timedelta(days=10))
+    vervaldatum_koek = fields.Date(string="Vervaldatum van de koek", required=True, help="Vul hier de vervaldatum van de koek in")
     categorie_koek = fields.Selection(
         string="Categorie van de koek",
         selection=[
