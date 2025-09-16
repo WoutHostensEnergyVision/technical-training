0a0cca45d16cc8b93c53464ccfa2830f66e702e4
Wout Hostens <wout.hostens@energyvision.be>
2025-09-16 13:30:18 +0200

    rebuild version

--- Bakker/__manifest__.py
+++ Bakker/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Bakker",  # The name that will appear in the App list
-    "version": "18.0.1.0.4",  # Version
+    "version": "18.0.1.0.5",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
