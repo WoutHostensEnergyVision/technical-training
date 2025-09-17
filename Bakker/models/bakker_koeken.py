9978a37e532183ac9178879ef8963d4c78249db4
Wout Hostens <wout.hostens@energyvision.be>
2025-09-17 10:45:45 +0200

    added categoriev2

--- Bakker/models/bakker_koeken.py
+++ Bakker/models/bakker_koeken.py
@@ -13,13 +13,20 @@ class BakkerKoeken(models.Model):
     aankoopdatum_koek = fields.Date(string="Aankoopdatum van de koek", default=fields.Date.today, help="Vul hier de aankoopdatum van de koek in", readonly=True)
     categorie_koek_id = fields.Many2one('bakker_koeken_categorie', string="Categorie van de koek", required=True, help="Selecteer hier de categorie van de koek")
     pudding_koek = fields.Boolean(string="Bevat pudding", default=False, help="Vink dit aan als de koek pudding bevat")
-    totaal_inventarisatie = fields.Float(string="Totale inventarisatie waarde", compute="_compute_totaal_inventarisatie", store=True, readonly=True)
+    totaal_inventarisatie = fields.Float(string="Totale inventarisatie waarde", compute="_compute_totaal_inventarisatie", store=True, readonly=True, inverse="_inverse_totaal_inventarisatie", help="Totale waarde van de koek in inventarisatie (prijs * voorraad)")
     tags_ids = fields.Many2many('bakker_koeken_tags', string="Tags", help="Selecteer hier de tags voor de koek")
     
     @api.depends('prijs_koek', 'voorraad_koek')
     def _compute_totaal_inventarisatie(self):
         for record in self:
             record.totaal_inventarisatie = record.prijs_koek * record.voorraad_koek
+    
+    def _inverse_totaal_inventarisatie(self):
+        for record in self:
+            if record.prijs_koek != 0:
+                record.voorraad_koek = record.totaal_inventarisatie / record.prijs_koek
+            else:
+                record.voorraad_koek = 0
             
     @api.constrains('voorraad_koek', 'prijs_koek')
     def _check_non_negative(self):
